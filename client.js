4be4c0845f6d9bab1281c1a8d25c424c95812b6d
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-10-08 20:25:22 -0600

    initializing variables

---
+++ client.js
@@ -0,0 +1,8 @@
+function clickCounter() {
+  // Defines html tags as variables to be used inside function
+  const addOne = document.querySelector(".countUp");
+  const removeOne = document.querySelector(".countDown");
+  const counterElement = document.querySelector(".counted");
+  // Counter variable
+  let counter = 0;
+}
