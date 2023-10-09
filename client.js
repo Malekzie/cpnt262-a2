25c12011e78504710f4ebb8ddb087eae826759ec
Robbie <133696122+Malekzie@users.noreply.github.com>
2023-10-09 17:34:16 -0600

    added more to the button clicker, now can use an inputed number as an exponent to boost the number

--- client.js
+++ client.js
@@ -2,18 +2,28 @@ function clickCounter() {
   const addOne = document.querySelector(".countUp");
   const downOne = document.querySelector(".countDown");
   const counterElement = document.querySelector(".counted");
-
+  const inputElement = document.getElementById("input");
+  const reset = document.querySelector(".reset");
   let count  = 0;
 
+  // Resets the count to zero
+  function resetCount(){
+    count = 0;
+    updateCount();
+  }
+
+  // Applies the changes into the count variable which is then applied to the span containing the count
   function updateCount() {
     counterElement.textContent = count;
   }
   
+  // Increases count by 1
   function incrementOne() {
     count++;
     updateCount();
   }
 
+  // Decreases count by 1
   function decrementOne() {
     if (count > 0){
       count--;
@@ -21,12 +31,22 @@ function clickCounter() {
     } else{
       alert("Count too low! STOP THAT");
     }
-  }
+    }
 
+    document.getElementById("exponent-form").addEventListener("submit", function(event){
+      event.preventDefault();
+        const exponent = parseFloat(inputElement.value)
+        const expand = exponent.toExponential();
+        count *= expand;
+        updateCount();
+      })
+
+  reset.addEventListener("click", resetCount);
   addOne.addEventListener("click", incrementOne);
   downOne.addEventListener("click", decrementOne);
 }
 
+// String Concat function
 document.getElementById("string-game-form").addEventListener("submit", function(event){
   event.preventDefault();
 
